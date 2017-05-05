4126d68022e884aba746a792962a7df7d2fdf558
Darrell Teo <darrelltzj@gmail.com>
2017-05-05 13:43:12 +0800

    master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504070821) do
+ActiveRecord::Schema.define(version: 20170504070852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 20170504070821) do
   create_table "flights_users", id: false, force: :cascade do |t|
     t.integer "flight_id", null: false
     t.integer "user_id",   null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.date     "dob"
+    t.integer  "flight_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_passengers_on_flight_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +58,5 @@ ActiveRecord::Schema.define(version: 20170504070821) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "passengers", "flights"
 end
